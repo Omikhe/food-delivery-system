5f1662333d745a04eb2ea498bacfbbe73da52b32
nutchavelli <bsc-com-06-22@unima.ac.mw>
2024-05-16 19:20:36 +0200

    adjusted styles

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,6 +1,8 @@
 
 import NavBar from "./Navbar";
 import { useState } from "react";
+import { MdOutlineArrowBackIos } from "react-icons/md";
+import food from "../images/food.jpg";
 
 
 
@@ -16,9 +18,11 @@ const Payment=()=>{
         <>
         <NavBar />
         {/* div supposed to be linked to previous page*/}
-        <p className="ml-40 mb-1.5 font-bold">~ Back </p>
-        <div className="flex bg-gray-100 items-center ml-40 mr-40  justify-around min-h-96">
-            <img src="" alt="image" className="h-3/5 "/>
+        <p className="ml-40 mb-1.5 font-bold flex content-center">
+            <MdOutlineArrowBackIos className="mr-2"/>
+            Back </p>
+        <div className="flex bg-gray-100 items-center ml-40 mr-40  justify-around min-h-96 rounded-lg">
+            <img src={food} alt="payImage" className="h-80  w-80 rounded-lg"/>
             <div className="flex-column pr-1 min-w-16">
                 <p className="font-bold mb-5 text-base">Choose payment method</p>
                 <form>
@@ -32,12 +36,12 @@ const Payment=()=>{
                     <div className="mb-2">
                         <input type="checkbox" onChange={()=>handleCheckBoxClick("second")} checked={selectedMethod==="second" }/>
                         <label className="font-normal ml-1">
-                            option 1
+                            option 2
                         </label>
                     </div>
                     
                 </form>
-                <button className="bg-orange-500 py-1 px-2 text-white mt-5 min-w-14">
+                <button className="bg-orange-500 py-1 px-2 text-white mt-5 w-20">
                         Proceed
                 </button>
             </div>

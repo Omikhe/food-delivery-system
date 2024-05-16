d2b2ce32b23ba450b35a72b3ceaa03ce9413dfa5
WILSONCHINYAMA <bsc-com-16-22@unima.ac.mw>
2024-05-16 21:21:40 +0200

    added initial structure of login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,10 +1,26 @@
 
-function Login(){
-    return(
-        <>
-        <h1>hello</h1>
+function Login() {
+    return (
+        <body class="bg-gray-100 h-screen flex items-center justify-center">
+    <div class="bg-white p-8 rounded shadow-md w-96">
+        <h2 class="text-2xl font-semibold mb-4">Login</h2>
+        <form action="#" method="POST">
+            <div class="mb-4">
+                <label for="username" class="block text-sm font-medium text-gray-700">Username</label>
+                <input type="text" id="username" name="username" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md" />
+            </div>
+            <div class="mb-4">
+                <label for="password" class="block text-sm font-medium text-gray-700">Password</label>
+                <input type="password" id="password" name="password" class="mt-1 focus:ring-indigo-500 focus:border-indigo-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"/>
+            </div>
+            <div class="flex items-center justify-between">
+                <button type="submit" class="bg-orange-600 text-white px-4 py-2 rounded-md hover:bg-indigo-600 focus:outline-none focus:bg-indigo-600">Login</button>
+                <a href="#" class="text-sm text-orange-600 hover:text-orange-700">Forgot Password?</a>
+            </div>
+        </form>
+    </div>
+</body>
         
-        </>
     )
 
 };

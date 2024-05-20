c07a2db9a7a0e03bcb0885fe57fd4bfae914720f
Caroline-Nkhoma <bsc-com-35-22@unima.ac.mw>
2024-05-20 22:48:47 +0200

    re-implement checkout page on new branch

--- src/components/checkout.js
+++ src/components/checkout.js
@@ -1,27 +1,25 @@
 import { Link } from "react-router-dom"
-import FOODIES from "./images/FOOODIES.jpg"
-function Checkout () {
-    return(
-        <>
-        <div className="bg-white-400 rounded-3xl w-full mt-28 ml-3">
-            <div className="flex-column w-fit grid grid-cols-2 w-3/4 mr-1">
-                <div className="col-span-1">
-                    <br/>
-                    <p className="ml-5 text-3xl">Chicken</p>
-                    <p className="ml-5 text-3xl">Price: MK5,400</p>
-                    <p className="ml-5 text-3xl">Delivery Fee: MK1,000</p>
-                    <p className="ml-5 text-3xl"></p>
-                    <br></br>
-                    <Link className="p-5 ml-4 border rounded-3xl border-blue-400 bg-blue-700 text-white ">PROCEED</Link>
-                </div>
-                
-                <div className="w-3/4 col-span-1">
-                    <img src={FOODIES} alt="food" className=" w-inherit  h-full rounded-3xl mt-42" width={800} height={850}/>
-                </div>
-                
 
-            
-            </div>
+function Checkout () {
+
+    return(
+
+        <>
+{/* adds a border to the picture */} 
+<div className=" px-60 "> 
+        <div className="flex justify-between border  border-box bg-gray-200 rounded-3xl h-96 mt-28">
+            {/* aligning components in column formatn */}
+        <div className=" w-96 h-96 bg-orange-300 mr-10"> 
+        <img src="#" alt="meal here"/>
+        </div>
+        <div className="w-96 h-96 bg-green-300">
+            <h1>Burger</h1>
+            <h2>MWK2,500</h2>
+            <p>gahkdkjhaskjdhakjda kahkdahdakjhakjdah das ahskdjhskjhdakd hsdakhiuyiryiuewyruiyeiuyriue</p>
+            <button>Proceed</button>
+        </div>
+
+        </div>
         </div>
     
         </>

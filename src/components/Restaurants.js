571a3298eb5acfed071273d648c5ecec982da0f6
Omikhe <msakamike84@gmail.com>
2024-05-20 21:16:58 +0200

    change branch name

--- src/components/Restaurants.js
+++ src/components/Restaurants.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Navbar from './Navbar';
 import img from './images/img.png';
+import { BrowserRouter as Router, Link } from "react-router-dom";
+
 
 //helper function for the open/close sign according to time of the day
 function nthawi() {
@@ -57,44 +59,50 @@ function RestaurantCard(props) {
                 </div>
                 <div className="ml-5">
                     <h1 className="font-semibold mb-3">{props.name}</h1>
-                    <h2 className="mb-3 text-sm text-gray-500">Chikanda - Near Bridge</h2>
+                    <h2 className="mb-3 text-sm text-gray-500">{props.location}</h2>
                     <div className="flex items-center text-sm">
                         <OpenClose></OpenClose>
                     </div>
                 </div>
             </div>
-            <button className="w-full border-white p-4 text-white bg-orange-600 rounded-lg">View Menu</button>
+            <button className="w-full border-white p-4 text-white bg-orange-600 rounded-lg">
+
+
+                <Link to="/Menu">View Menu</Link>
+
+            </button>
         </div>
     </>;
 }
 
 
-
+//component to dislpay CTA message
 function Message() {
     return <>
         <h1 className="h-10 pl-60 pr-60 pt-5 pb-20 text-bold text-5xl font-bold">Choose your restaurant</h1>
     </>;
 }
 
+//main component to render the cards and messages
 function Restaurants() {
     return <div className="bg-gray-50 ">
 
         <Navbar></Navbar>
         <Message />
         <div className="flex h-full pl-60 pr-60 pt-5 justify-around">
-            <RestaurantCard name={"Bossman Restaurant"}></RestaurantCard>
-            <RestaurantCard name={"Akuzike Foods"} />
-            <RestaurantCard name={"Steers Chikanda"} />
+            <RestaurantCard name={"Bossman Restaurant"} location={"Chikanda - Near Matiya"}></RestaurantCard>
+            <RestaurantCard name={"Akuzike Foods"} location={"Chikanda - Near Matiya"} />
+            <RestaurantCard name={"Steers Chikanda"} location={"Chikanda - Near Matiya"} />
         </div>
         <div className="flex h-full pl-60 pr-60 pt-5 justify-around">
-            <RestaurantCard name={"ChiefChef Delicacy"} />
-            <RestaurantCard name={"Undercroft Cafe"} />
-            <RestaurantCard name={"Aunt Nina Cafe"} />
+            <RestaurantCard name={"ChiefChef Delicacy"} location={"Chikanda - Near Matiya"} />
+            <RestaurantCard name={"Undercroft Cafe"} location={"Chikanda - Near Matiya"} />
+            <RestaurantCard name={"Aunt Nina Cafe"} location={"Chikanda - Near Matiya"} />
         </div>
         <div className="flex h-full pl-60 pr-60 pt-5 pb-20 justify-around">
-            <RestaurantCard name={"Makawa Restaurant"} />
-            <RestaurantCard name={"UNIMA Food Haven"} />
-            <RestaurantCard name={"UNIMA H/Eco Cafe"} />
+            <RestaurantCard name={"Makawa Restaurant"} location={"Chikanda - Near Matiya"} />
+            <RestaurantCard name={"UNIMA Food Haven"} location={"Chikanda - Near Matiya"} />
+            <RestaurantCard name={"UNIMA H/Eco Cafe"} location={"Chikanda - Near Matiya"} />
         </div>
     </div>;
 }

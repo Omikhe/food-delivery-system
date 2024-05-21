562654ae16a2368bd15e190a22ec7099aab83da0
nutchavelli <bsc-com-06-22@unima.ac.mw>
2024-05-21 16:22:26 +0200

    adjusted some styles and added icons

--- src/components/Payment.js
+++ src/components/Payment.js
@@ -1,42 +1,47 @@
 
 import NavBar from "./Navbar";
-import { useState } from "react";
+
 import { MdOutlineArrowBackIos } from "react-icons/md";
+import { BiSolidBank } from "react-icons/bi";
+
 import food from "./images/burger.png";
 
 
 
 const Payment=()=>{
 
-    const[selectedMethod,setSelected]=useState("first");
-
-    function handleCheckBoxClick(selected) {
-        setSelected(selected);
+    function handleSubmit(e){
+        e.preventDefault();
+        //handling payment submission code
     }
 
     return(
         <>
         <NavBar />
         {/* div supposed to be linked to previous page*/}
-        <p className="ml-40 mb-1.5 font-bold flex content-center">
-            <MdOutlineArrowBackIos className="mr-2"/>
-            Back </p>
+        <p className="ml-40 mb-1.5 font-bold flex items-center">
+            <MdOutlineArrowBackIos className="mr-2 h-3"/>
+            Back 
+        </p>
         <div className="flex bg-gray-100 items-center ml-40 mr-40  justify-around min-h-96  rounded-lg">
             <img src={food} alt="payImage" className="h-80  w-80 rounded-lg"/>
             <div className="flex-column pr-1 min-w-16">
                 <p className="font-bold mb-5 text-base">Choose payment method</p>
-                <form>
-                    <div className="mb-2">
-                        <input type="checkbox" onChange={()=>handleCheckBoxClick("first")} checked={selectedMethod==="first"}/>
+
+                <form   onSubmit={(e=>handleSubmit(e))}>
+                    <div className="mb-2 flex items-center">
+                        <input type="radio" name="pay-method"/>
+                        <BiSolidBank className="bg-black h-5 w-5 text-white ml-3 text-sm rounded"/>
                         <label className="font-normal ml-1">
-                            option 1
+                            PayChangu
                         </label>
                     </div>
-                    <hr className="font-semibold mb-2"/>
-                    <div className="mb-2">
-                        <input type="checkbox" onChange={()=>handleCheckBoxClick("second")} checked={selectedMethod==="second" }/>
+                    <hr className="font-semibold mb-2 mx-3"/>
+                    <div className="mb-2 flex items-center">
+                        <input type="radio" name="pay-method"/>
+                        <BiSolidBank className="bg-black h-5 w-5 text-white ml-3 rounded"/>
                         <label className="font-normal ml-1">
-                            option 2
+                            Airtel money
                         </label>
                     </div>
                     <button className="bg-orange-600 py-1 px-2 text-white mt-5 w-20">

f3d76593cbe25f901f877f588e850c6e389e5783
Omikhe <msakamike84@gmail.com>
2024-05-21 08:17:36 +0200

    add hover effect on sign up button

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,11 +13,17 @@ function Navbar() {
                     <ul className="flex w-1/3 justify-between font-normal">
                         <li className="p-5"><Link to="/Home">Home</Link></li>
                         <li className="p-5"><Link to="/Restaurants">Restaurants</Link></li>
-                        <li className="p-5"><Link to="#">About</Link></li>
+                        <li className="p-5"><Link to="/About">About</Link></li>
                     </ul>
+
+                    {/* TODO: Add conditional rendering to these elements */}
                     <div className="ml-5">
-                        <button className="px-5 py-2.5 border-2 border-gray-800 text-xl text-black ml-5 bg-transparent rounded-lg">Sign Up</button>
-                        <button className="px-5 py-2.5 border-2 border-gray-800 text-xl text-white ml-5 bg-gray-900 rounded-lg">Log In</button>
+                        <button className="px-5 py-2.5 border-2 border-gray-800 text-xl hover:bg-gray-900 hover:text-white text-black ml-5 bg-transparent rounded-lg">
+                            <Link to="/Signup">Sign Up</Link>
+                        </button>
+                        <button className="px-5 py-2.5 border-2 border-gray-800 text-xl text-white ml-5 bg-gray-900 rounded-lg">
+                            <Link to="/Login">Log In</Link>
+                        </button>
                     </div>
                 </div>
             </nav>

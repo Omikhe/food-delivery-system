344874838897f23ba09648d5361d457558806ee0
bed-com-04-22 <bed-com-04-22@unima.ac.mw>
2024-05-26 20:13:38 +0200

    menu-page updated file

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,31 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import restaurantsData from './Restaurants.json';
+import { OpenClose } from "./Restaurants";
+import { Link, useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
-import logo from './images/steers-chikanda-logo.png';
-import burger from './images/burger.png';
-import chipsChicken from './images/chips-chicken.png';
-import coffeeTea from './images/coffee-tea.png';
-import coffee from './images/coffee.png';
-import pizza from './images/pizza.png';
-import riceBeans from './images/rice-beans.png';
-import riceChicken from './images/rice-chicken.png';
-import salad from './images/salad.png';
-import spaghettiMeatBalls from './images/spaghetti-meat-balls.png';
-import spaghettiSoya from './images/spaghetti-soya.png';
 
 function RestaurantDetails({ logo, name, location }) {
   return (
     <div className="flex items-center gap-10 mt-10">
       <div className="w-40 h-40 rounded-3xl overflow-hidden bg-black">
-        <img src={logo} alt="Restaurant Logo" />
+        <img src={require(`${logo}`)} alt="Restaurant Logo" />
       </div>
       <div>
         <div className="flex flex-col">
           <h1 className="text-5xl font-bold">{name}</h1>
           <p>{location}</p>
           <div className="flex items-center">
-            <div className="w-3 h-3 rounded-full bg-green-500 mr-1"></div>
-            <p className="text-green-600">Open</p>
+            <OpenClose />
           </div>
         </div>
       </div>
@@ -46,14 +36,14 @@ function MenuItem({ image, name, price }) {
   return (
     <div className="flex flex-col w-64 h-80 mr-8 p-6 justify-between items-center bg-gray-200 text-black text-xl rounded-2xl">
       <div className="w-full h-full rounded-3xl p-0 mb-1 overflow-hidden bg-transparent">
-        <img src={image} alt={name} className="w-full h-full  object-cover" />
+        <img src={require(`${image}`)} alt={name} className="w-full h-full  object-cover" />
       </div>
       <div className="flex flex-col items-center mb-2 mt-0">
         <h2 className="font-bold text-base">{name}</h2>
         <p className="text-center">{price}</p>
       </div>
       <Link to="/Checkout" className="w-full">
-        <button className="w-full p-2 bg-orange-600 text-white rounded-md">
+        <button className="w-full p-2 bg-orange-600 hover:bg-orange-500 text-white rounded-md">
           Order
         </button>
       </Link>
@@ -62,38 +52,60 @@ function MenuItem({ image, name, price }) {
 }
 
 function Menu() {
+  const location = useLocation();
+  const selectedRestaurant = location.state?.menu
+    ? restaurantsData.find((restaurant) => restaurant.name === location.state.name)
+    : null;
+
+  // Render the component only if a restaurant is selected
+  if (!selectedRestaurant) {
+    return (
+      <>
+        <Navbar />
+        <div className="h-10 pl-60 pr-60 pt-5 pb-20 text-bold text-center text-5xl font-bold">
+          Please select a restaurant to view the menu.
+        </div>
+      </>
+    );
+  }
+
+  const renderMenuItems = () => {
+    const menuItemRows = [];
+    const numRows = Math.ceil(selectedRestaurant.Menu.length / 5);
+
+    for (let i = 0; i < numRows; i++) {
+      const rowMenuItems = selectedRestaurant.Menu.slice(i * 5, i * 5 + 5).map((item, index) => (
+        <MenuItem
+          key={index}
+          image={item.image}
+          name={item.name}
+          price={item.price}
+        />
+      ));
+
+      menuItemRows.push(
+        <div key={i} className="flex px-60 mb-8">
+          {rowMenuItems}
+        </div>
+      );
+    }
+
+    return menuItemRows;
+  };
+
   return (
     <>
       <Navbar />
       <div className="px-60">
-        <RestaurantDetails logo={logo} name={"Steers Chikanda"} location={"Chikanda - Near Matiya"} />
+        <RestaurantDetails
+          logo={selectedRestaurant.logo}
+          name={selectedRestaurant.name}
+          location={selectedRestaurant.location}
+        />
         <CallToAction />
       </div>
 
-      <div className="flex px-60">
-        <MenuItem image={burger} name={"Burger"} price={"MWK2,500"}/>
-        <MenuItem image={pizza} name={"Pizza"} price={"MWK4,500"}/>
-        <MenuItem image={spaghettiMeatBalls} name={"Spaghetti & Meatballs"} price={"MWK3,500"}/>
-        <MenuItem image={spaghettiSoya} name={"Spaghetti & Soya Pieces  "} price={"MWK3,500"}/>
-        <MenuItem image={chipsChicken} name={"Chips & Chicken"} price={"MWK3,500"}/>
-      </div>
-
-      <div className="flex px-60 mt-8">
-        <MenuItem image={salad} name={"Greek Salad"} price={"MWK1,000"}/>
-        <MenuItem image={coffeeTea} name={"Coffee & Cake"} price={"MWK4,500"}/>
-        <MenuItem image={coffee} name={"Burger"} price={"MWK1,500"}/>
-        <MenuItem image={riceChicken} name={"Burger"} price={"MWK1,500"}/>
-        <MenuItem image={riceBeans} name={"Burger"} price={"MWK1,500"}/>
-      </div>
-
-      <div className="flex px-60 mt-8">
-        <MenuItem image={burger} name={"Burger"} price={"MWK1,500"}/>
-        <MenuItem image={burger} name={"Burger"} price={"MWK1,500"}/>
-        <MenuItem image={burger} name={"Burger"} price={"MWK1,500"}/>
-        <MenuItem image={burger} name={"Burger"} price={"MWK1,500"}/>
-        <MenuItem image={burger} name={"Burger"} price={"MWK1,500"}/>
-      </div>
-
+      {renderMenuItems()}
     </>
   );
 }

8676b7ab96e9c8cfff84939bb8a57ebbc37454c5
Omikhe <msakamike84@gmail.com>
2024-11-19 22:58:27 +0200

    add hero content

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,22 @@
 import Navbar from "./components/layout/navbar"
 export default function Home() {
   return (
-    <Navbar />
+    <>
+      <Navbar />
+      <div className="flex flex-col justify-center items-center h-screen">
+        <div className="flex flex-col items-center w-[350px] text-center">
+          <div className="text-[32px] mb-4">
+            <h1 className="font-bold">Hungry?</h1>
+            <h1>Let Us Bring the Food to You!</h1>
+          </div>
+          <h2 className="text-[16px] mb-6">
+            Simplify Your Meals With Chirunga Munchies Food Delivery System
+          </h2>
+          <button className="w-[109.53px] h-[45px] bg-[#FF6C44] text-[#ffffff] font-semibold rounded-[5px]">
+            Order Now
+          </button>
+        </div>
+      </div>
+    </>
   );
 }

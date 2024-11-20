cf89a42f15917689e75cb5ad1cf34d980c2fe91f
Omikhe <msakamike84@gmail.com>
2024-11-20 23:59:41 +0200

    move files around

---
+++ app/restaurants/meals/page.tsx
@@ -0,0 +1,12 @@
+import MealCard from "../components/meal-card";
+
+
+export default function Page() {
+    return (
+        <>
+            <div className="flex flex-col justify-between items-start py-6 px-5 max-w-7xl mx-auto w-full">
+                <MealCard />
+            </div>
+        </>
+    );
+}

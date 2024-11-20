dd870e284f11b0d42f8bf233fd558bfba2438980
Omikhe <msakamike84@gmail.com>
2024-11-21 00:07:03 +0200

    import navbar

--- app/restaurants/meals/page.tsx
+++ app/restaurants/meals/page.tsx
@@ -1,10 +1,12 @@
-import MealCard from "../../components/restaurant-card"
+import MealCard from "@/app/components/meal-card"
+import Navbar from "@/app/components/layout/navbar";
 
 
 
 export default function Page() {
     return (
         <>
+            <Navbar></Navbar>
             <div className="flex flex-col justify-between items-start py-6 px-5 max-w-7xl mx-auto w-full">
                 <MealCard />
             </div>

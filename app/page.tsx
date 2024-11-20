89c65ed0ea3775ce46773e9e0aeff04c87d7593f
Omikhe <msakamike84@gmail.com>
2024-11-20 20:01:53 +0200

    add more styles to the hero

--- app/page.tsx
+++ app/page.tsx
@@ -5,15 +5,15 @@ export default function Home() {
     <>
       <Navbar />
       <div className="flex flex-col justify-center items-center h-screen">
-        <div className="flex flex-col items-center w-[350px] text-center">
-          <div className="text-[32px] mb-4">
-            <h1 className="font-bold">Hungry?</h1>
-            <h1>Let Us Bring the Food to You!</h1>
+        <div className="flex flex-col items-center w-[350px] text-center lg:w-[761px] lg:h-[343px]">
+          <div className="text-[32px] mb-4 lg:text-[64px] lg:mb-[3px]">
+            <h1><b>Hungry?</b> Let Us </h1>
+            <h1>Bring the Food to You!</h1>
           </div>
-          <h2 className="text-[16px] mb-6">
-            Simplify Your Meals With Chirunga Munchies Food Delivery System
+          <h2 className="text-[16px] mb-6 lg:mb-[39px]">
+            Simplify Your Meals With <b>Chirunga Munchies</b> Food Delivery System
           </h2>
-          <button className="w-[109.53px] h-[45px] bg-[#FF6C44] text-[#ffffff] font-semibold rounded-[5px]">
+          <button className="w-[109.53px] h-[45px] bg-[#FF6C44] text-[#ffffff] font-semibold rounded-[5px] lg:w-[129px] lg:h-[53px] lg:text-[18.28px] lg:hover:bg-[#ED5A32]">
             Order Now
           </button>
         </div>
@@ -22,6 +22,7 @@ export default function Home() {
           width={246}
           height={246}
           alt="a top shot of food in a plate"
+          className="lg:w-[391px] lg:h-[391px]"
         />
       </div>
     </>

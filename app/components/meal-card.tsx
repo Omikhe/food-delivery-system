8f3db638eb777483a54de9839b92111919636942
Omikhe <msakamike84@gmail.com>
2024-11-21 11:34:40 +0200

    styles meal card

--- app/components/meal-card.tsx
+++ app/components/meal-card.tsx
@@ -1,7 +1,21 @@
-export default function Page() {
+import Link from "next/link";
+
+
+export default function MealCard() {
     return (
-        <>
-            <div>Choose your fave restaurant!😉</div>
-        </>
+        <div className="flex flex-col w-64 h-80 mr-8 p-6 justify-between items-center bg-gray-200 text-black text-xl rounded-2xl">
+            <div className="w-full h-full rounded-3xl p-0 mb-1 overflow-hidden bg-transparent">
+
+            </div>
+
+            <div className="flex flex-col items-center mb-2 mt-0">
+                <h2 className="font-bold text-base">Signature Burger</h2>
+                <p className="text-center">MWK2,500</p>
+            </div>
+
+            <Link className="w-full" href="/Checkout">
+                <button className="w-full p-2 bg-[#FF6C44] hover:bg-orange-500 text-white rounded-md">Order</button>
+            </Link>
+        </div>
     );
 }

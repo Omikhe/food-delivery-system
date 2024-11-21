0743b1832984211258c258187fb1e346d94008ab
Omikhe <msakamike84@gmail.com>
2024-11-21 10:00:35 +0200

    add right orange hex

--- app/components/restaurant-card.tsx
+++ app/components/restaurant-card.tsx
@@ -1,18 +1,18 @@
 export default function RestaurantCard() {
     return (
-        <div className="flex flex-col w-[344px] h-[163px] bg-[#F6F5FA] justify-center items-start p-[17px] rounded-[5px]">
-            <div className="flex w-[257.63px] h-[73.13px] mb-[20px]">
-                <div className="w-[81.02px] h-[73.13px] bg-[#BCBDC1] mr-[17px] rounded-[5px]"></div>
-                <div>
-                    <h1 className="font-semibold text-[15px] mb-1">Bossman Restaurant</h1>
-                    <h2 className="font-normal text-[10px] text-[#BCBDC1] mb-1">Chikanda • Near Bridge</h2>
-                    <div className="flex items-center">
-                        <div className="w-[5px] h-[5px] rounded-full bg-[#27AE61] mr-1"></div>
-                        <h2 className="text-[#27AE61] font-medium text-[10px]">Open</h2>
+        <div className="flex flex-col w-[460px] h-[272px] bg-[#F6F5FA] justify-center items-start p-10 rounded-[5px] text-xl">
+            <div className="flex w-[380px] h-[112px] mb-[20px]">
+                <div className="w-[112px] h-[112px] bg-[#BCBDC1] mr-[17px] rounded-[5px]"></div>
+                <div className="">
+                    <h1 className="font-semibold mb-3">Bossman Restaurant</h1>
+                    <h2 className="mb-3 text-sm text-gray-500">Chikanda • Near Bridge</h2>
+                    <div className="flex items-center text-sm">
+                        <div className="w-3 h-3 rounded-full bg-green-500  mr-1"></div>
+                        <h2 className="text-[#27AE61] font-medium">Open</h2>
                     </div>
                 </div>
             </div>
-            <button className="w-[304px] h-[40px] bg-[#FF6C44] text-[#ffffff] font-semibold rounded-[5px] text-[13px] hover:bg-[#ED5A32]">
+            <button className="w-full border-white p-4 text-white bg-[#FF6C44] hover:bg-orange-500 rounded-lg">
                 View Menu
             </button>
         </div>

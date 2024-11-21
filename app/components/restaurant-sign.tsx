dc0dc11a28553c390737207a6f7aa019727bcc78
Omikhe <msakamike84@gmail.com>
2024-11-21 11:09:34 +0200

    compotentize restaurant sign

---
+++ app/components/restaurant-sign.tsx
@@ -0,0 +1,16 @@
+export default function RestaurantSign() {
+    return (
+        <div className="flex gap-10 items-center">
+            <div className="w-40 h-40 bg-[#BCBDC1] rounded-3xl overflow-hidden"></div>
+
+            <div>
+                <h1 className="text-5xl font-bold mb-1">Bossman Restaurant</h1>
+                <h2 className="font-normal text-[#BCBDC1] mb-1">Chikanda • Near Bridge</h2>
+                <div className="flex items-center">
+                    <div className="w-3 h-3 rounded-full bg-[#27AE61] mr-1"></div>
+                    <h2 className="text-[#27AE61] font-medium">Open</h2>
+                </div>
+            </div>
+        </div>
+    );
+}

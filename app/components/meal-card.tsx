febcdda40a9cc50f57c9da9b2feca18f5b66885d
Omikhe <msakamike84@gmail.com>
2024-11-23 00:26:37 +0200

    break styling

--- app/components/meal-card.tsx
+++ app/components/meal-card.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function MealCard() {
     return (
-        <div className="flex flex-col w-64 h-80 p-6 justify-between items-center bg-[#F6F5FA] text-black text-xl rounded-2xl">
+        <div className="flex flex-col w-[166px] h-[182px] lg:w-64 lg:h-80 p-6 justify-between items-center bg-[#F6F5FA] text-black text-xl rounded-2xl">
             <div className="flex items-center justify-center w-full h-full rounded-3xl p-0 mb-1 overflow-hidden bg-transparent">
                 <Image
                     src={"/burger.png"}
@@ -15,12 +15,12 @@ export default function MealCard() {
             </div>
 
             <div className="flex flex-col items-center mb-2 mt-0">
-                <h2 className="font-bold text-base">Signature Burger</h2>
-                <p className="text-center">MWK2,500</p>
+                <h2 className="text-nowrap text-sm font-bold lg:text-base">Signature Burger</h2>
+                <p className="text-sm lg:text-base text-center">MWK2,500</p>
             </div>
 
-            <Link className="w-full" href="/">
-                <button className="w-full p-2 bg-[#FF6C44] hover:bg-orange-500 text-white rounded-md">Order</button>
+            <Link className=" lg:text-base w-[133px] h-[29px] lg:w-full lg:h-60 p-2 bg-[#FF6C44] hover:bg-orange-500 text-white rounded-md" href="/">
+                <button className="">Order</button>
             </Link>
         </div>
     );

5e92168fc084cb8874c7fe1e07129cfd75e006ca
Omikhe <msakamike84@gmail.com>
2024-11-22 23:51:32 +0200

    change CTA margin bottom

--- app/restaurants/page.tsx
+++ app/restaurants/page.tsx
@@ -6,7 +6,7 @@ export default function Page() {
         <>
             <Navbar />
             <div className="flex flex-col justify-between items-start py-6 px-5 max-w-7xl mx-auto w-full">
-                <div className="w-[240px] h-[88px] text-[32px] mb-[16px] text-left font-bold lg:w-[600px] lg:text-[40px] lg:font-bold lg:mb-[48px]">Pick your fave restaurant! 😉</div>
+                <div className="w-[240px] h-[88px] text-[32px] mb-[64px] text-left font-bold lg:w-[600px] lg:text-[40px] lg:font-bold">Pick your fave restaurant! 😉</div>
                 <div className="columns-1 lg:gap-[18px] lg:columns-3">
 
                     <div className="mb-[16px] lg:mb-[18px] lg:break-inside-avoid">

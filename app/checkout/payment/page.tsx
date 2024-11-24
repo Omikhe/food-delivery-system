da312591607836b02295c6bfe3cc89d6145f367b
Omikhe <msakamike84@gmail.com>
2024-11-24 20:29:26 +0200

    add basic styles

---
+++ app/checkout/payment/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/app/components/layout/navbar"
+
+export default function Page() {
+    return (
+        <>
+            <Navbar></Navbar>
+            <div className="flex flex-col justify-between items-center py-6 px-5 max-w-7xl mx-auto w-full overflow-hidden md:h-full md:flex md:flex-row md:items-center md:justify-start md:gap-32 md:py-10 md:bg-[#F6F5FA] rounded-xl">
+                mike
+            </div>
+        </>
+    );
+}

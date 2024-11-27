659d76ec43941890b5db4a1a9545e03148cadcb0
Omikhe <msakamike84@gmail.com>
2024-11-27 13:31:28 +0200

    implement signup

---
+++ app/signup/page.tsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import Navbar from "../components/layout/navbar";
+export default function Page() {
+    return (
+        <div>
+            <Navbar></Navbar>
+            <main className="flex flex-col justify-between items-start py-6 px-5 max-w-7xl mx-auto w-full">
+
+                <div className="my-20">Logo</div>
+                <h1 className="font-semibold text-4xl mb-4">Sign Up</h1>
+                <form action="" className="w-full">
+
+                    <div className="flex flex-col text-base mb-4">
+                        <label htmlFor="email">Email</label>
+                        <input className="bg-[#DDDDDD] w-full h-16 p-4 rounded-lg" type="email" name="email" />
+                    </div>
+                    <div className="flex flex-col text-base mb-4">
+                        <label htmlFor="password">Password</label>
+                        <input className="bg-[#DDDDDD] w-full h-16 p-4 rounded-lg" type="password" name="password" />
+                    </div>
+                    <div className="flex flex-col text-base mb-1">
+                        <label htmlFor="password">Confirm Password</label>
+                        <input className="bg-[#DDDDDD] w-full h-16 p-4 rounded-lg" type="password" name="password" />
+                    </div>
+                    <Link href={"#"} className="flex mb-8">Forgot Password?</Link>
+
+                    <button className="w-full h-14 bg-[#FF6C44] active:bg-orange-500 text-base font-semibold rounded-lg text-white">
+                        Sign Up
+                    </button>
+
+                </form>
+            </main>
+            <div className="flex items-center justify-center gap-1">
+                Already have an account? <Link href={"/login"}>Log in</Link>
+            </div>
+        </div>
+    );
+}
